46719e270ca135d6d4d02ae9638fb7d2bf0955c5
wubo <13003373077@163.com>
2018-08-14 21:02:20 +0800

    增加二次修改票号消息推送代码

--- qunar-parent/qunar-order/src/main/java/com/apin/qunar/order/service/national/impl/UpdateTicketNoServiceImpl.java
+++ qunar-parent/qunar-order/src/main/java/com/apin/qunar/order/service/national/impl/UpdateTicketNoServiceImpl.java
@@ -3,9 +3,13 @@ package com.apin.qunar.order.service.national.impl;
 import com.apin.qunar.order.dao.impl.NationalOrderDaoImpl;
 import com.apin.qunar.order.domain.national.updateTicketNo.UpdateTicketNoDTO;
 import com.apin.qunar.order.service.national.UpdateTicketNoService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class UpdateTicketNoServiceImpl implements UpdateTicketNoService {
     @Autowired
     NationalOrderDaoImpl nationalOrderDao;
