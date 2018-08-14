4674d4fefa6f7ead860b2e843c83c76943e6636e
wubo <13003373077@163.com>
2018-08-14 21:15:23 +0800

    修改报价类型名称

--- qunar-parent/qunar-order/src/main/java/com/apin/qunar/order/service/international/impl/NtsSearchPriceServiceImpl.java
+++ qunar-parent/qunar-order/src/main/java/com/apin/qunar/order/service/international/impl/NtsSearchPriceServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
@@ -37,16 +38,52 @@ public class NtsSearchPriceServiceImpl extends NtsApiService<NtsSearchPriceParam
     @Override
     public ApiResult<NtsSearchPriceResultVO> searchPrice(final NtsSearchPriceParam ntsPriceSearchParam, final String merchantNo) {
         ApiResult<NtsSearchPriceResultVO> apiResult = execute(ntsPriceSearchParam);
-        NtsSearchPriceResultVO ntsSearchPriceResult = apiResult.getResult();
         if (apiResult == null) {
             return ApiResult.fail();
         }
+        NtsSearchPriceResultVO ntsSearchPriceResult = apiResult.getResult();
         if (!apiResult.isSuccess()) {
             log.warn("查询国际航班报价异常,param:{},原因:{}", JSON.toJSON(ntsPriceSearchParam), apiResult.getMessage());
             return ApiResult.fail(apiResult.getCode(), "航班信息发生变动，请重新搜索");
         }
         ntsSearchPriceResult.setPriceInfo(ntsSearchPriceResult.getPriceInfo().stream().sorted(Comparator.comparing(NtsSearchPriceResultVO.PriceInfo::getPrice)).collect(Collectors.toList()));
 //        ntsFlightPriceRedis.setFlightPriceInfo(ntsPriceSearchParam, ntsSearchPriceResult);
+        changePackName(ntsSearchPriceResult);
         return apiResult;
+    }
+    private void changePackName(NtsSearchPriceResultVO ntsSearchPriceResultVO) {
+        List<NtsSearchPriceResultVO.PriceInfo> priceInfos = ntsSearchPriceResultVO.getPriceInfo();
+        for(NtsSearchPriceResultVO.PriceInfo priceInfo : priceInfos)
+        {
+            switch (priceInfo.getPackName())
+            {
+                case "去哪儿自营" :
+                    priceInfo.setPackName("爱拼自营");
+                    break;
+                case "商旅套餐" :
+                    priceInfo.setPackName("爱拼惠盈");
+                    break;
+                case "商务舱" :
+                    priceInfo.setPackName("爱拼商务舱");
+                    break;
+                case "低价特惠" :
+                    priceInfo.setPackName("爱拼国际特优");
+                    break;
+                case "低价专享" :
+                    priceInfo.setPackName("爱拼国际包优");
+                    break;
+                case "头等舱" :
+                    priceInfo.setPackName("爱拼头等舱");
+                    break;
+                case "旗舰店" :
+                    priceInfo.setPackName("爱拼旗舰店");
+                    break;
+                case "去哪儿旗舰店" :
+                    priceInfo.setPackName("爱拼旗舰店");
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
