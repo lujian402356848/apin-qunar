49972b54efe1699f62d73c9fc62e9fa9bbfd34a9
ligang <1239418110@qq.com>
2018-08-27 17:01:05 +0800

    删除order下couponExtMapper

--- qunar-parent/qunar-order/src/main/java/com/apin/qunar/order/dao/mapper/CouponExtMapper.java
+++
@@ -1,9 +0,0 @@
-package com.apin.qunar.order.dao.mapper;
-
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Update;
-
-public interface CouponExtMapper {
-    @Update("update coupon set status=#{Status} where id=#{Id} and status!=#{Status}")
-    int updateStatus(@Param("id") Long id, @Param("status") int status);
-}
